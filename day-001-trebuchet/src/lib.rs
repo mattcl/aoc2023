4131c0587ad3d706bb76eb4321df6a0380066c53
Matt Chun-Lum <matt@questionable.engineering>
2023-12-01 02:15:41 -0800

    perf(day1): avoid vec allocations

--- day-001-trebuchet/src/lib.rs
+++ day-001-trebuchet/src/lib.rs
@@ -3,69 +3,62 @@ use std::str::FromStr;
 use aoc_plumbing::Problem;
 
 fn extract_digit_from_slice(slice: &[u8]) -> u32 {
-    if slice.starts_with(b"one") || slice.starts_with(b"1") {
+    if slice[0].is_ascii_digit() {
+        (slice[0] - b'0') as u32
+    } else if slice.starts_with(b"one") {
         1
-    } else if slice.starts_with(b"two") || slice.starts_with(b"2") {
+    } else if slice.starts_with(b"two") {
         2
-    } else if slice.starts_with(b"three") || slice.starts_with(b"3") {
+    } else if slice.starts_with(b"three") {
         3
-    } else if slice.starts_with(b"four") || slice.starts_with(b"4") {
+    } else if slice.starts_with(b"four") {
         4
-    } else if slice.starts_with(b"five") || slice.starts_with(b"5") {
+    } else if slice.starts_with(b"five") {
         5
-    } else if slice.starts_with(b"six") || slice.starts_with(b"6") {
+    } else if slice.starts_with(b"six") {
         6
-    } else if slice.starts_with(b"seven") || slice.starts_with(b"7") {
+    } else if slice.starts_with(b"seven") {
         7
-    } else if slice.starts_with(b"eight") || slice.starts_with(b"8") {
+    } else if slice.starts_with(b"eight") {
         8
-    } else if slice.starts_with(b"nine") || slice.starts_with(b"9") {
+    } else if slice.starts_with(b"nine") {
         9
-    } else if slice.starts_with(b"0") {
-        0
     } else {
         u32::MAX
     }
 }
 
-fn sum_lines(lines: &[Vec<u32>]) -> u32 {
-    let mut sum = 0;
-    for line in lines.iter() {
-        sum += match line.as_slice() {
-            [] => 0,
-            [a] => a * 10 + a,
-            [a, .., b] => a * 10 + b,
-        };
-    }
-    sum
-}
-
 #[derive(Debug, Clone)]
 pub struct Trebuchet {
-    lines: Vec<Vec<u32>>,
-    letter_lines: Vec<Vec<u32>>,
+    lines: u32,
+    letter_lines: u32,
 }
 
 impl FromStr for Trebuchet {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut lines = Vec::default();
-        let mut letter_lines = Vec::default();
+        let mut lines = 0;
+        let mut letter_lines = 0;
 
-        for (idx, l) in s.trim().split('\n').enumerate() {
-            lines.push(
-                l.chars()
-                    .filter_map(|ch| ch.to_digit(10))
-                    .collect::<Vec<_>>(),
-            );
-            letter_lines.push(Vec::new());
+        for l in s.trim().split('\n') {
+            let digits = l
+                .chars()
+                .filter_map(|ch| ch.to_digit(10))
+                .collect::<Vec<_>>();
+            lines += match digits.as_slice() {
+                [] => 0,
+                [a] => a * 10 + a,
+                [a, .., b] => a * 10 + b,
+            };
+
+            let mut sum = 0;
             let bytes = l.as_bytes();
             for i in 0..bytes.len() {
                 let slice = &bytes[i..];
                 let v = extract_digit_from_slice(slice);
                 if v != u32::MAX {
-                    letter_lines[idx].push(v);
+                    sum += v * 10;
                     break;
                 }
             }
@@ -73,10 +66,11 @@ impl FromStr for Trebuchet {
                 let slice = &bytes[(bytes.len() - 1 - i)..];
                 let v = extract_digit_from_slice(slice);
                 if v != u32::MAX {
-                    letter_lines[idx].push(v);
+                    sum += v;
                     break;
                 }
             }
+            letter_lines += sum;
         }
 
         Ok(Self {
@@ -96,11 +90,13 @@ impl Problem for Trebuchet {
     type P2 = u32;
 
     fn part_one(&mut self) -> Result<Self::P1, Self::ProblemError> {
-        Ok(sum_lines(&self.lines))
+        // it was easier to solve this as part of the parsing
+        Ok(self.lines)
     }
 
     fn part_two(&mut self) -> Result<Self::P2, Self::ProblemError> {
-        Ok(sum_lines(&self.letter_lines))
+        // it was easier to solve this as part of the parsing
+        Ok(self.letter_lines)
     }
 }
 
