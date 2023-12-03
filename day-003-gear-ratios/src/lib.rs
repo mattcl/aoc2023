43bcaea1033d0906a100863fe9141da88c91ab7d
Matt Chun-Lum <matt@questionable.engineering>
2023-12-02 23:10:36 -0800

    perf(day3): reduce duplication and construct numbers without vecs

--- day-003-gear-ratios/src/lib.rs
+++ day-003-gear-ratios/src/lib.rs
@@ -1,57 +1,52 @@
-use std::{collections::VecDeque, str::FromStr};
+use std::str::FromStr;
 
 use aoc_plumbing::Problem;
 use aoc_std::{collections::Grid, directions::BoundedCardinalNeighbors, geometry::Location};
 use rustc_hash::{FxHashMap, FxHashSet};
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Number {
-    value: u32,
-    start: usize,
-    end: usize,
-}
-
-impl Number {
-    pub fn contains(&self, col: usize) -> bool {
-        self.start <= col && self.end >= col
-    }
-}
-
 #[derive(Debug, Clone)]
 pub struct GearRatios {
     chars: Grid<char>,
-    candidates: FxHashSet<Location>,
-    potential_gears: FxHashMap<Location, Vec<Location>>,
+    symbol_map: FxHashMap<char, Vec<Vec<Location>>>,
 }
 
 impl GearRatios {
-    // find all the symbols and then check all candidate spaces around them for
-    // numbers
-    pub fn find_numbers(&self) -> u32 {
-        let mut processed: FxHashSet<Location> = FxHashSet::default();
-
-        let mut total_sum = 0;
-
-        // we need to turn all the candidate locations into numbers
-        'outer: for can in self.candidates.iter() {
+    // this is wonky, but there was more variance in this "common" functionality
+    // than I originally thought.
+    fn extract_numbers<'a, I>(
+        &self,
+        iter: I,
+        total: &mut u32,
+        target_count: u32,
+        processed: &mut FxHashSet<Location>,
+        sum: bool,
+    ) -> bool
+    where
+        I: Iterator<Item = &'a Location>,
+    {
+        let mut count = 0;
+        'outer: for can in iter {
             if processed.contains(can) {
                 continue;
             }
+
             processed.insert(*can);
-            let mut number = VecDeque::default();
 
+            let mut digits: u32 = 1;
             // we know this is safe
-            number.push_back(self.chars.get(can).unwrap().to_digit(10).unwrap());
+            let mut number: u32 = self.chars.get(can).unwrap().to_digit(10).unwrap();
 
+            // walk to the west
             let mut working = *can;
             while let Some(west) = working.west() {
                 if let Some(v) = self.chars.get(&west) {
                     if processed.contains(&west) {
                         continue 'outer;
                     }
-                    if v.is_digit(10) {
+                    if v.is_ascii_digit() {
                         working = west;
-                        number.push_front(v.to_digit(10).unwrap());
+                        number += v.to_digit(10).unwrap() * 10_u32.pow(digits);
+                        digits += 1;
                         processed.insert(west);
                         continue;
                     }
@@ -59,15 +54,16 @@ impl GearRatios {
                 break;
             }
 
+            // walk to the east
             let mut working = *can;
             while let Some(east) = working.east() {
                 if let Some(v) = self.chars.get(&east) {
                     if processed.contains(&east) {
                         continue 'outer;
                     }
-                    if v.is_digit(10) {
+                    if v.is_ascii_digit() {
                         working = east;
-                        number.push_back(v.to_digit(10).unwrap());
+                        number = number * 10 + v.to_digit(10).unwrap();
                         processed.insert(east);
                         continue;
                     }
@@ -75,83 +71,54 @@ impl GearRatios {
                 break;
             }
 
-            // make the number from the end to the start
-            let mut sum = 0;
-            for i in 0..number.len() {
-                sum = sum * 10 + number[i];
+            if sum {
+                *total += number;
+            } else {
+                *total *= number;
             }
+            count += 1;
+            if target_count > 0 && count > target_count {
+                return false;
+            }
+        }
 
-            total_sum += sum;
+        count == target_count
+    }
+
+    pub fn find_part_numbers(&self) -> u32 {
+        let mut processed: FxHashSet<Location> = FxHashSet::default();
+
+        let mut total_sum = 0;
+
+        // we need to turn all the candidate locations into numbers
+        for svs in self.symbol_map.values() {
+            self.extract_numbers(
+                svs.iter().flatten(),
+                &mut total_sum,
+                0,
+                &mut processed,
+                true,
+            );
         }
 
         total_sum
     }
 
-    pub fn find_gears(&self) -> u32 {
+    pub fn find_gear_numbers(&self) -> u32 {
         let mut total_sum = 0;
 
-        for (_gear, gear_candidates) in self.potential_gears.iter() {
+        for gear_candidates in self.symbol_map.get(&'*').unwrap().iter() {
             let mut processed: FxHashSet<Location> = FxHashSet::default();
-            let mut nums = Vec::default();
+            let mut sub_product = 1;
 
-            // we need to turn all the candidate locations into numbers
-            'outer: for can in gear_candidates {
-                if processed.contains(can) {
-                    continue;
-                }
-                processed.insert(*can);
-                let mut number = VecDeque::default();
-
-                // we know this is safe
-                number.push_back(self.chars.get(can).unwrap().to_digit(10).unwrap());
-
-                let mut working = *can;
-                while let Some(west) = working.west() {
-                    if let Some(v) = self.chars.get(&west) {
-                        if processed.contains(&west) {
-                            continue 'outer;
-                        }
-                        if v.is_digit(10) {
-                            working = west;
-                            number.push_front(v.to_digit(10).unwrap());
-                            processed.insert(west);
-                            continue;
-                        }
-                    }
-                    break;
-                }
-
-                let mut working = *can;
-                while let Some(east) = working.east() {
-                    if let Some(v) = self.chars.get(&east) {
-                        if processed.contains(&east) {
-                            continue 'outer;
-                        }
-                        if v.is_digit(10) {
-                            working = east;
-                            number.push_back(v.to_digit(10).unwrap());
-                            processed.insert(east);
-                            continue;
-                        }
-                    }
-                    break;
-                }
-
-                // make the number from the end to the start
-                let mut sum: u32 = 0;
-                for i in 0..number.len() {
-                    sum = sum * 10 + number[i];
-                }
-
-                nums.push(sum);
-
-                if nums.len() > 2 {
-                    break;
-                }
-            }
-
-            if nums.len() == 2 {
-                total_sum += nums.into_iter().product::<u32>();
+            if self.extract_numbers(
+                gear_candidates.iter(),
+                &mut sub_product,
+                2,
+                &mut processed,
+                false,
+            ) {
+                total_sum += sub_product
             }
         }
 
@@ -165,32 +132,26 @@ impl FromStr for GearRatios {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let chars: Grid<char> = Grid::from_str(s)?;
 
-        let mut candidates = FxHashSet::default();
-        let mut potential_gears: FxHashMap<Location, Vec<Location>> = FxHashMap::default();
+        let mut symbol_map: FxHashMap<char, Vec<Vec<Location>>> = FxHashMap::default();
 
         for row in 0..chars.height() {
             for col in 0..chars.width() {
                 let loc = Location::new(row, col);
                 let s = chars.locations[row][col];
-                if !(s.is_digit(10) || s == '.') {
+                if !(s.is_ascii_digit() || s == '.') {
+                    let symbol_entry = symbol_map.entry(s).or_default();
+                    let mut loc_entry = Vec::default();
                     for (_, n, ch) in chars.neighbors(&loc) {
-                        if ch.is_digit(10) {
-                            candidates.insert(n);
-                            if s == '*' {
-                                let e = potential_gears.entry(loc).or_default();
-                                e.push(n);
-                            }
+                        if ch.is_ascii_digit() {
+                            loc_entry.push(n);
                         }
                     }
+                    symbol_entry.push(loc_entry);
                 }
             }
         }
 
-        Ok(Self {
-            chars,
-            candidates,
-            potential_gears,
-        })
+        Ok(Self { chars, symbol_map })
     }
 }
 
@@ -204,11 +165,11 @@ impl Problem for GearRatios {
     type P2 = u32;
 
     fn part_one(&mut self) -> Result<Self::P1, Self::ProblemError> {
-        Ok(self.find_numbers())
+        Ok(self.find_part_numbers())
     }
 
     fn part_two(&mut self) -> Result<Self::P2, Self::ProblemError> {
-        Ok(self.find_gears())
+        Ok(self.find_gear_numbers())
     }
 }
 
