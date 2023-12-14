ab1d94b3f2c0d1be4d8c54164e386e69559a71af
Matt Chun-Lum <matt@questionable.engineering>
2023-12-14 03:45:33 -0800

    perf(day14): interval bucketing

--- day-014-parabolic-reflector-dish/src/lib.rs
+++ day-014-parabolic-reflector-dish/src/lib.rs
@@ -1,38 +1,38 @@
-use std::{fmt::Display, str::FromStr};
+use std::str::FromStr;
 
 use aoc_plumbing::Problem;
-use aoc_std::{directions::Cardinal, geometry::{Interval, Point2D}, collections::Grid};
+use aoc_std::{collections::Grid, geometry::Interval};
 use itertools::Itertools;
-use rustc_hash::{FxHashMap, FxHashSet};
-use rayon::prelude::*;
+use rustc_hash::FxHashMap;
 
 #[derive(Debug, Default, Clone)]
-pub struct Dish2 {
-    rounds: Vec<Point2D<i8>>,
+pub struct Dish {
     row_intervals: Vec<Vec<Interval<i8>>>,
     col_intervals: Vec<Vec<Interval<i8>>>,
-    rounds_in_rows: Vec<Vec<usize>>,
-    rounds_in_cols: Vec<Vec<usize>>,
+    rounds_in_rows: Vec<Vec<i8>>,
+    rounds_in_cols: Vec<Vec<i8>>,
 }
 
-impl Dish2 {
-    fn total_load(&self) -> u32 {
-        let height = self.row_intervals.len() as u32;
-        self.rounds.iter().map(|r| height - r.x as u32).sum()
+impl Dish {
+    fn total_load_p1(&self) -> u32 {
+        let height = self.rounds_in_rows.len();
+        self.rounds_in_rows
+            .iter()
+            .enumerate()
+            .map(|(i, r)| (r.len() * (height - i)) as u32)
+            .sum()
     }
 
     pub fn cycle(&mut self, count: usize) -> u32 {
         let mut cache: FxHashMap<u128, usize> = FxHashMap::default();
         let mut loads: Vec<u32> = Vec::with_capacity(500);
-        let mut buckets = vec![vec![]; 100];
         for cycle_idx in 0..count {
-            self.tilt_north(&mut buckets);
-            self.tilt_west(&mut buckets);
-            self.tilt_south(&mut buckets);
-            self.tilt_east(&mut buckets);
-            let load = self.total_load();
+            self.tilt_north();
+            self.tilt_west();
+            self.tilt_south();
+            let load = self.tilt_east();
 
-            // make a key with the last 8 loads
+            // make a key with the last ~8~ 4 loads
             if cycle_idx > 4 {
                 let key: u128 = loads[(loads.len() - 5)..]
                     .iter()
@@ -47,11 +47,7 @@ impl Dish2 {
                 if *e != cycle_idx {
                     let period = cycle_idx - *e;
 
-                    dbg!("potential");
-
                     if count % period == cycle_idx % period {
-                        dbg!(&self.row_intervals);
-                        dbg!(cycle_idx);
                         let rem = (count - cycle_idx) % period;
                         // we need to advance by rem in loads from the last index - 1
                         return loads[*e + rem - 1];
@@ -62,101 +58,99 @@ impl Dish2 {
             loads.push(load);
         }
 
-        self.total_load()
+        // we should never make it here
+        unreachable!("how did this happen?")
     }
 
-    fn tilt_north(&mut self, buckets: &mut Vec<Vec<usize>>) {
+    fn tilt_north(&mut self) {
         for (col, intervals) in self.col_intervals.iter().enumerate() {
-            for round_idx in self.rounds_in_cols[col].drain(..) {
-                for (interval_idx, interval) in intervals.iter().enumerate() {
-                    if interval.contains_value(self.rounds[round_idx].x) {
-                        buckets[interval_idx].push(round_idx);
-                    }
-                }
-            }
+            // we know the intervals are in sorted order
+            let mut interval_idx = 0;
+            let mut interval_insert_count = 0;
+            // by the nature of how we insert things, these should always be
+            // sorted (because the intervals are also in order)
+            // self.rounds_in_cols[col].sort();
 
-            for (interval_idx, bucket) in buckets.iter_mut().enumerate() {
-                for (p, round_idx) in bucket.drain(..).enumerate() {
-                    // move ourself (it doesn't matter the order)
-                    self.rounds[round_idx].x = intervals[interval_idx].start + p as i8;
-                    // prep for the next cycle
-                    self.rounds_in_rows[self.rounds[round_idx].x as usize].push(round_idx);
+            for value in self.rounds_in_cols[col].drain(..) {
+                while !intervals[interval_idx].contains_value(value) {
+                    interval_idx += 1;
+                    interval_insert_count = 0;
                 }
+                // start assigning values from the start of the interval and
+                // working up
+                let new_value = intervals[interval_idx].start + interval_insert_count;
+                self.rounds_in_rows[new_value as usize].push(col as i8);
+                interval_insert_count += 1;
             }
         }
     }
 
-    fn tilt_south(&mut self, buckets: &mut Vec<Vec<usize>>) {
+    fn tilt_south(&mut self) {
         for (col, intervals) in self.col_intervals.iter().enumerate() {
-            for round_idx in self.rounds_in_cols[col].drain(..) {
-                for (interval_idx, interval) in intervals.iter().enumerate() {
-                    if interval.contains_value(self.rounds[round_idx].x) {
-                        buckets[interval_idx].push(round_idx);
-                    }
-                }
-            }
+            let mut interval_idx = 0;
+            let mut interval_insert_count = 0;
 
-            for (interval_idx, bucket) in buckets.iter_mut().enumerate() {
-                for (p, round_idx) in bucket.drain(..).enumerate() {
-                    // move ourself (it doesn't matter the order)
-                    self.rounds[round_idx].x = intervals[interval_idx].end - p as i8;
-                    // prep for the next cycle
-                    self.rounds_in_rows[self.rounds[round_idx].x as usize].push(round_idx);
+            for value in self.rounds_in_cols[col].drain(..) {
+                while !intervals[interval_idx].contains_value(value) {
+                    interval_idx += 1;
+                    interval_insert_count = 0;
                 }
+                // start assigning values from the end of the interval and
+                // working down
+                let new_value = intervals[interval_idx].end - interval_insert_count;
+                self.rounds_in_rows[new_value as usize].push(col as i8);
+                interval_insert_count += 1;
             }
         }
     }
 
-    fn tilt_east(&mut self, buckets: &mut Vec<Vec<usize>>) {
+    fn tilt_west(&mut self) {
         for (row, intervals) in self.row_intervals.iter().enumerate() {
-            for round_idx in self.rounds_in_rows[row].drain(..) {
-                for (interval_idx, interval) in intervals.iter().enumerate() {
-                    if interval.contains_value(self.rounds[round_idx].y) {
-                        buckets[interval_idx].push(round_idx);
-                    }
-                }
-            }
+            let mut interval_idx = 0;
+            let mut interval_insert_count = 0;
 
-            for (interval_idx, bucket) in buckets.iter_mut().enumerate() {
-                for (p, round_idx) in bucket.drain(..).enumerate() {
-                    // move ourself (it doesn't matter the order)
-                    self.rounds[round_idx].y = intervals[interval_idx].end - p as i8;
-                    // prep for the next cycle
-                    self.rounds_in_cols[self.rounds[round_idx].y as usize].push(round_idx);
+            for value in self.rounds_in_rows[row].drain(..) {
+                while !intervals[interval_idx].contains_value(value) {
+                    interval_idx += 1;
+                    interval_insert_count = 0;
                 }
+                let new_value = intervals[interval_idx].start + interval_insert_count;
+                self.rounds_in_cols[new_value as usize].push(row as i8);
+                interval_insert_count += 1;
             }
         }
     }
 
-    fn tilt_west(&mut self, buckets: &mut Vec<Vec<usize>>) {
+    /// we can caluclate the north load after finishing the east tilt without
+    /// having to iterate through _again._
+    fn tilt_east(&mut self) -> u32 {
+        let mut load = 0;
+        let height = self.rounds_in_rows.len() as u32;
         for (row, intervals) in self.row_intervals.iter().enumerate() {
-            for round_idx in self.rounds_in_rows[row].drain(..) {
-                for (interval_idx, interval) in intervals.iter().enumerate() {
-                    if interval.contains_value(self.rounds[round_idx].y) {
-                        buckets[interval_idx].push(round_idx);
-                    }
-                }
-            }
+            let mut interval_idx = 0;
+            let mut interval_insert_count = 0;
 
-            for (interval_idx, bucket) in buckets.iter_mut().enumerate() {
-                for (p, round_idx) in bucket.drain(..).enumerate() {
-                    // move ourself (it doesn't matter the order)
-                    self.rounds[round_idx].y = intervals[interval_idx].start + p as i8;
-                    // prep for the next cycle
-                    self.rounds_in_cols[self.rounds[round_idx].y as usize].push(round_idx);
+            for value in self.rounds_in_rows[row].drain(..) {
+                while !intervals[interval_idx].contains_value(value) {
+                    interval_idx += 1;
+                    interval_insert_count = 0;
                 }
+                let new_value = intervals[interval_idx].end - interval_insert_count;
+                load += height - row as u32;
+                self.rounds_in_cols[new_value as usize].push(row as i8);
+                interval_insert_count += 1;
             }
         }
+        load
     }
 }
 
-
-impl FromStr for Dish2 {
+impl FromStr for Dish {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let grid: Grid<char> = Grid::from_str(s)?;
-        let mut dish = Dish2 {
+        let mut dish = Dish {
             rounds_in_rows: vec![vec![]; grid.height()],
             rounds_in_cols: vec![vec![]; grid.width()],
             ..Default::default()
@@ -171,14 +165,13 @@ impl FromStr for Dish2 {
                     '#' => {
                         col_interval_markers[col].push(row as i8);
                         interval_markers.push(col as i8);
-                    },
+                    }
                     'O' => {
                         // we start always with NORTH, so we want cols first
                         // dish.rounds_in_rows[row].push(dish.rounds.len());
-                        dish.rounds_in_cols[col].push(dish.rounds.len());
-                        dish.rounds.push((row as i8, col as i8).into());
+                        dish.rounds_in_cols[col].push(row as i8);
                     }
-                    _ => {},
+                    _ => {}
                 }
             }
             interval_markers.push(grid.width() as i8);
@@ -209,190 +202,18 @@ impl FromStr for Dish2 {
     }
 }
 
-
-#[derive(Debug, Clone)]
-pub struct Dish {
-    rounds: Vec<u128>,
-    cubes: Vec<u128>,
-    width: usize,
-}
-
-impl Default for Dish {
-    fn default() -> Self {
-        Self {
-            rounds: Vec::with_capacity(100),
-            cubes: Vec::with_capacity(100),
-            width: 0,
-        }
-    }
-}
-
-impl Dish {
-    pub fn tilt(&mut self, dir: &Cardinal) {
-        match dir {
-            Cardinal::North => {
-                for c in 0..self.width {
-                    let mask = 1 << c;
-                    let mut target = 0;
-                    for r in 0..self.rounds.len() {
-                        if self.cubes[r] & mask != 0 {
-                            target = r + 1;
-                        } else if self.rounds[r] & mask != 0 {
-                            // remove this round from this row
-                            self.rounds[r] &= !mask;
-                            self.rounds[target] |= mask;
-                            target += 1;
-                        }
-                    }
-                }
-            }
-            Cardinal::West => {
-                for r in 0..self.rounds.len() {
-                    let mut target = self.width - 1;
-                    for c in (0..self.width).rev() {
-                        let mask = 1 << c;
-                        if self.cubes[r] & mask != 0 && c != 0 {
-                            target = c - 1;
-                        } else if self.rounds[r] & mask != 0 {
-                            // remove this round from this row
-                            self.rounds[r] &= !mask;
-                            self.rounds[r] |= 1 << target;
-                            if target != 0 {
-                                target -= 1;
-                            }
-                        }
-                    }
-                }
-            }
-            Cardinal::South => {
-                for c in 0..self.width {
-                    let mask = 1 << c;
-                    let mut target = self.rounds.len() - 1;
-                    for r in (0..self.rounds.len()).rev() {
-                        if self.cubes[r] & mask != 0 && r != 0 {
-                            target = r - 1;
-                        } else if self.rounds[r] & mask != 0 {
-                            // remove this round from this row
-                            self.rounds[r] &= !mask;
-                            self.rounds[target] |= mask;
-                            if target != 0 {
-                                target -= 1;
-                            }
-                        }
-                    }
-                }
-            }
-            Cardinal::East => {
-                for r in 0..self.rounds.len() {
-                    let mut target = 0;
-                    for c in 0..self.width {
-                        if self.rounds[r] >> c == 0 {
-                            break;
-                        }
-                        let mask = 1 << c;
-                        if self.cubes[r] & mask != 0 {
-                            target = c + 1;
-                        } else if self.rounds[r] & mask != 0 {
-                            // remove this round from this row
-                            self.rounds[r] &= !mask;
-                            self.rounds[r] |= 1 << target;
-                            target += 1;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    pub fn cycle(&mut self, count: usize) -> u32 {
-        let mut cache: FxHashMap<u128, usize> = FxHashMap::default();
-        let mut loads: Vec<u32> = Vec::default();
-        for cycle_idx in 0..count {
-            self.tilt(&Cardinal::North);
-            self.tilt(&Cardinal::West);
-            self.tilt(&Cardinal::South);
-            self.tilt(&Cardinal::East);
-            let load = self.total_load();
-
-            // make a key with the last 8 loads
-            if cycle_idx > 4 {
-                let key: u128 = loads[(loads.len() - 5)..]
-                    .iter()
-                    .fold(0, |acc, v| (acc << 32 | *v as u128));
-                // let key_b: u128 = loads[(loads.len() - 9)..(loads.len() - 4)]
-                //     .iter()
-                //     .fold(0, |acc, v| (acc << 32 | *v as u128));
-                // let key = (key_a, key_b);
-
-                let e = cache.entry(key).or_insert(cycle_idx);
-
-                if *e != cycle_idx {
-                    let period = cycle_idx - *e;
-
-                    if count % period == cycle_idx % period {
-                        let rem = (count - cycle_idx) % period;
-                        // we need to advance by rem in loads from the last index - 1
-                        return loads[*e + rem - 1];
-                    }
-                }
-            }
-
-            loads.push(load);
-        }
-
-        self.total_load()
-    }
-
-    pub fn total_load(&self) -> u32 {
-        let mut total = 0;
-        for r in 0..self.rounds.len() {
-            total += self.rounds[r].count_ones() as u32 * (self.rounds.len() - r) as u32;
-        }
-        total
-    }
-}
-
-impl Display for Dish {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in self.rounds.iter() {
-            writeln!(f, "{:020b}", row)?;
-        }
-
-        Ok(())
-    }
-}
-
 #[derive(Debug, Clone)]
 pub struct ParabolicReflectorDish {
     dish: Dish,
-    dish2: Dish2,
 }
 
 impl FromStr for ParabolicReflectorDish {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut dish = Dish::default();
+        let dish = Dish::from_str(s)?;
 
-        dish.width = s.lines().next().unwrap().len();
-
-        for line in s.trim().lines() {
-            let mut cubes = 0;
-            let mut rounds = 0;
-            for (c, ch) in line.chars().enumerate() {
-                match ch {
-                    '#' => cubes |= 1 << dish.width - c - 1,
-                    'O' => rounds |= 1 << dish.width - c - 1,
-                    _ => {}
-                }
-            }
-            dish.cubes.push(cubes);
-            dish.rounds.push(rounds);
-        }
-
-        let dish2 = Dish2::from_str(s)?;
-
-        Ok(Self { dish, dish2 })
+        Ok(Self { dish })
     }
 }
 
@@ -406,14 +227,13 @@ impl Problem for ParabolicReflectorDish {
     type P2 = u32;
 
     fn part_one(&mut self) -> Result<Self::P1, Self::ProblemError> {
-        let mut dish = self.dish2.clone();
-        let mut buckets = vec![vec![]; 100];
-        dish.tilt_north(&mut buckets);
-        Ok(dish.total_load())
+        let mut dish = self.dish.clone();
+        dish.tilt_north();
+        Ok(dish.total_load_p1())
     }
 
     fn part_two(&mut self) -> Result<Self::P2, Self::ProblemError> {
-        Ok(self.dish2.cycle(1_000_000_000))
+        Ok(self.dish.cycle(1_000_000_000))
     }
 }
 
@@ -429,7 +249,6 @@ mod tests {
         let input = std::fs::read_to_string("input.txt").expect("Unable to load input");
         let solution = ParabolicReflectorDish::solve(&input).unwrap();
         assert_eq!(solution, Solution::new(105982, 85175));
-        assert!(false);
     }
 
     #[test]
