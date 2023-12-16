4aec146a771ff16377c0d4ec26f7d37f29cb7a46
Matt Chun-Lum <matt@questionable.engineering>
2023-12-15 16:36:29 -0800

    perf(day14): revisit the bitwise solution

--- day-014-parabolic-reflector-dish/src/lib.rs
+++ day-014-parabolic-reflector-dish/src/lib.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{collections::VecDeque, str::FromStr};
 
 use aoc_plumbing::Problem;
 use aoc_std::{collections::Grid, geometry::Interval};
@@ -14,7 +14,7 @@ pub struct Dish {
 }
 
 impl Dish {
-    fn total_load_p1(&self) -> u32 {
+    pub fn total_load_p1(&self) -> u32 {
         let height = self.rounds_in_rows.len();
         self.rounds_in_rows
             .iter()
@@ -200,16 +200,175 @@ impl FromStr for Dish {
     }
 }
 
+// It's surprising that this is only slightly faster.
+#[derive(Debug, Default, Clone)]
+pub struct BitDish {
+    rounds: Vec<u128>,
+    cubes: Vec<u128>,
+    height: usize,
+    left_border_mask: u128,
+    right_border_mask: u128,
+}
+
+impl BitDish {
+    fn total_load(&self) -> u32 {
+        self.rounds
+            .iter()
+            .enumerate()
+            .map(|(i, r)| (self.height - i) as u32 * r.count_ones())
+            .sum()
+    }
+
+    pub fn cycle(&mut self, count: usize) -> u32 {
+        let mut cache: FxHashMap<u128, usize> = FxHashMap::default();
+        let mut loads: Vec<u32> = Vec::with_capacity(500);
+        for cycle_idx in 0..count {
+            self.tilt_north();
+            self.tilt_west();
+            self.tilt_south();
+            self.tilt_east();
+
+            let load = self.total_load();
+
+            loads.push(load);
+
+            // make a key with the last ~8~ 4 loads
+            if cycle_idx > 4 {
+                let key: u128 = loads[(loads.len() - 5)..]
+                    .iter()
+                    .fold(0, |acc, v| (acc << 32 | *v as u128));
+                // let key_b: u128 = loads[(loads.len() - 9)..(loads.len() - 4)]
+                //     .iter()
+                //     .fold(0, |acc, v| (acc << 32 | *v as u128));
+                // let key = (key_a, key_b);
+
+                let e = cache.entry(key).or_insert(cycle_idx);
+
+                if *e != cycle_idx {
+                    let period = cycle_idx - *e;
+
+                    let rem = (count - cycle_idx) % period;
+                    // we need to advance by rem in loads from the last index - 1
+                    return loads[*e + rem - 1];
+                }
+            }
+        }
+
+        // we should never make it here
+        unreachable!("how did this happen?")
+    }
+
+    fn tilt_north(&mut self) {
+        let mut rows = VecDeque::from_iter(1..self.height);
+
+        while let Some(row) = rows.pop_front() {
+            let target_row = row - 1;
+            let moves_available =
+                self.rounds[row] & !self.rounds[target_row] & !self.cubes[target_row];
+
+            if moves_available != 0 {
+                self.rounds[row] &= !moves_available;
+                self.rounds[target_row] |= moves_available;
+
+                if target_row > 0 {
+                    rows.push_front(target_row);
+                }
+            }
+        }
+    }
+
+    fn tilt_south(&mut self) {
+        let mut rows = Vec::from_iter(0..(self.height - 1));
+        while let Some(row) = rows.pop() {
+            let target_row = row + 1;
+            let moves_available =
+                self.rounds[row] & !self.rounds[target_row] & !self.cubes[target_row];
+
+            if moves_available != 0 {
+                self.rounds[row] &= !moves_available;
+                self.rounds[target_row] |= moves_available;
+
+                if target_row < self.height - 1 {
+                    rows.push(target_row);
+                }
+            }
+        }
+    }
+
+    fn tilt_west(&mut self) {
+        let mut rows = Vec::from_iter(0..self.height);
+        while let Some(row) = rows.pop() {
+            let cubes = self.cubes[row];
+            let rounds = self.rounds[row];
+            let moves_available = rounds & !((rounds | cubes) >> 1) & self.left_border_mask;
+            if moves_available != 0 {
+                self.rounds[row] = rounds & !moves_available | moves_available << 1;
+                rows.push(row);
+            }
+        }
+    }
+
+    fn tilt_east(&mut self) {
+        let mut rows = Vec::from_iter(0..self.height);
+        while let Some(row) = rows.pop() {
+            let cubes = self.cubes[row];
+            let rounds = self.rounds[row];
+            let moves_available = rounds & !((rounds | cubes) << 1) & self.right_border_mask;
+            if moves_available != 0 {
+                self.rounds[row] = rounds & !moves_available | moves_available >> 1;
+                rows.push(row);
+            }
+        }
+    }
+}
+
+impl FromStr for BitDish {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lines = s.lines().collect::<Vec<_>>();
+
+        let height = lines.len();
+        let width = lines[0].len();
+
+        let mut rounds = vec![0; height];
+        let mut cubes = vec![0; height];
+
+        for (row, line) in s.lines().enumerate() {
+            for (col, ch) in line.chars().enumerate() {
+                match ch {
+                    '#' => {
+                        cubes[row] |= 1 << (width - col - 1);
+                    }
+                    'O' => rounds[row] |= 1 << (width - col - 1),
+                    _ => {}
+                }
+            }
+        }
+
+        let left_border_mask = !(1 << (width - 1));
+        let right_border_mask = !1;
+
+        Ok(Self {
+            rounds,
+            cubes,
+            height,
+            left_border_mask,
+            right_border_mask,
+        })
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct ParabolicReflectorDish {
-    dish: Dish,
+    dish: BitDish,
 }
 
 impl FromStr for ParabolicReflectorDish {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let dish = Dish::from_str(s)?;
+        let dish = BitDish::from_str(s)?;
 
         Ok(Self { dish })
     }
@@ -227,7 +386,7 @@ impl Problem for ParabolicReflectorDish {
     fn part_one(&mut self) -> Result<Self::P1, Self::ProblemError> {
         let mut dish = self.dish.clone();
         dish.tilt_north();
-        Ok(dish.total_load_p1())
+        Ok(dish.total_load())
     }
 
     fn part_two(&mut self) -> Result<Self::P2, Self::ProblemError> {
